6942583ee9772b0b241ecb74803957b34dcbb88b
Frederick Kaakoye <fkaakoye001@st.ug.edu.gh>
2024-06-11 15:19:20 +0000

    The navebar isnt done yet and i am facing some issues

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,60 @@
 import React from 'react'
 import "./NavBar.css"
+import { Link } from 'react-router-dom'
+
 
 function NavBar() {
+    
   return (
-    <div>
-        <div className="nav">
-                <nav className="navbar">
-                    <div class="logoo">
-                        <image src='' />
-                    </div>    
-                        <ul class="nav-link">
-                            <li class="nav-linkss"><a href="/"> Home</a></li>
-                            <li class="nav-links"><a href="Services">Services</a></li>
-                            <li class="nav-links"><a href="Portfolio">Portfolio</a></li>
-                            <li class="nav-links"><a href="Contact">Contact</a></li>
-                            <li class="nav-links"><a href="About">About</a></li>
-                            <li class="nav-links"><a href="Bookings">Bookings</a></li>
+    
+    /* Header Section */
+    <header>
+      <div class="navbar">
+        <div class="logo"><a href="#">Construction Site</a></div>
+        <ul class="links">
+          <li><a href="src/routes/Home.js">Home</a></li>
+          <li><a href="src/routes/About.js">About</a></li>
+          <li><a href="src/routes/Services.js">Services</a></li>
+          <li><a href="src/routes/Portfolio.js">Portfolio</a></li>
+          <li><a href="src/routes/Contact.js">Contact</a></li>
+        </ul>
+        <a href="src/routes/Bookings.js" target="_blank" class="action_btn">Bookings</a>
+        <div class="toggle_btn">
+          <i class="fa-solid fa-bars"></i>
+        </div>
+      </div>
 
-                        </ul> 
-                </nav>
-            </div>
+      <div class="dropdown_menu">
+        <li><a href="src/routes/Home.js">Home</a></li>
+        <li><a href="src/routes/About.js">About</a></li>
+        <li><a href="src/routes/Services.js">Services</a></li>
+        <li><a href="src/routes/Portfolio.js">Portfolio</a></li>
+          <li><a href="src/routes/Contact.js">Contact</a></li>
+        <li><a href="src/routes/Bookings.js" target="_blank" class="action_btn">Bookings</a></li>
+      </div>
 
-
-    </div>
+      <main>
+        <section id="hero">
+          <h1>Welcome</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor at
+            voluptate alias eum aspernatur beatae. <br />
+            Consectetur animi labore voluptate atque, libero nostrum dicta
+            fugiat repellat!
+          </p>
+          <a href="bookanappointment.html" target="_blank" class="action_btn">Bookings</a>
+        </section>
+      </main>
+    </header>
+    
   )
+
 }
 
+<script src="/script.js"></script>
+
+
 export default NavBar
+
+
+    
