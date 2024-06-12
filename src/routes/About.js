b0e7a5f773b752a904d30aa5f0ddbd7bacdd5e3d
MENSAH ERASMUS <emensah670@st.ug.edu.gh>
2024-06-12 14:39:19 +0000

    Made changes

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,20 +1,13 @@
 import React from 'react'
 import "./About.css"
-import aboutPic from '../images/aboutPic.png'
+
 
 const About = () => {
  return (
 <div>
 
-<div className='container'>
-    <div className='picture'>
-      <img src={aboutPic} alt="Responsive" className='responsive-img' /> 
-    </div>
-  <div className='sub-heading'>
-      <h4 className='text2'> 
-       </h4>
-   </div>
-  </div>
+  About
+    
 </div>
   )
 }
