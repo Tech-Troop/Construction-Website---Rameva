6036192e816d9c2120e62432b175b1096be37b9c
Marks-Spencer20 <javisfrimpong.2003@gmail.com>
2024-06-12 03:47:12 +0000

    Working on the About page

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import "./About.css"
+import aboutPic from '../images/aboutPic.png'
 
-export default function About() {
-  return (
-    <div>About</div>
+const About = () => {
+ return (
+<div>
+
+<div className='container'>
+    <div className='picture'>
+      <img src={aboutPic} alt="Responsive" className='responsive-img' /> 
+    </div>
+  <div className='sub-heading'>
+      <h4 className='text2'> 
+       </h4>
+   </div>
+  </div>
+</div>
   )
 }
+export default About;
