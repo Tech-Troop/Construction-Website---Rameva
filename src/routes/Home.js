81b68948e7a53d9e8db07c3a8e41db0c228cf2c3
Frederick Kaakoye <fkaakoye001@st.ug.edu.gh>
2024-07-07 10:00:53 +0000

    I'm not done with home page but i had to push it to github becasue im transfering to a differnt machine

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,41 @@
 import React from 'react'
 import "./Home.css"
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+// import 'swiper/css';
+import 'swiper/css/navigation';
+// import './styles.css';
+
+// import required modules
+import { Navigation } from 'swiper/modules';
 
 function Home() {
   return (
-    <div className='homeContainer'>Home</div>
+    // Hero Section
+    <section className="hero swiper mySwiper">
+      <Swiper
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }}
+        modules={[Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 1</SwiperSlide>
+        
+      </Swiper>
+      <div className="hero-arrow row">
+        <div className="swiper-button-prev"></div>
+        <div className="swiper-button-next"></div>
+      </div>
+    </section>
+
+
+
+    // End Hero Section
   )
 }
 
