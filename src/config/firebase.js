fdeb9945f2a4b37ccae5367dba07437dd6bd8b9c
theboyGolden <preciousstick15@gmail.com>
2024-07-14 19:53:20 +0000

    Appointment page done- responsive checked, backend checked

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
@@ -15,4 +16,7 @@ const firebaseConfig = {
 
 
   const app = initializeApp(firebaseConfig);
+  const db =getFirestore(app);
+
+  export { db }
   export const auth = getAuth(app)
