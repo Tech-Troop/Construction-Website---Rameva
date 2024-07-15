d285456c2de2b5154c9e119caa976a8b306799a0
hawamicha <152286703+hawamicha@users.noreply.github.com>
2024-07-15 12:59:44 +0000

    update portfolio page

--- src/routes/Portfolio.js
+++ src/routes/Portfolio.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import "./Portfolio.css"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ProjectCard from '../components/portfolio/ProjectCard';
+import ContsructionPic from '../images/portfolio_images/contruction_pic.jpg';
+import projects from '../components/portfolio/ProjectData'
 
-function Portfolio() {
+const Portfolio = () => {
   return (
-    <div>Portfolio</div>
-  )
-}
+    <div className="portfolio">
+      <div className="hero-container">
+        <img src={ContsructionPic} alt="" className="hero-pic" />
+        <h1 className="hero-text">Portfolio</h1>
+      </div>
+      <h1 className="portfolio-title-text">OUR PROJECTS</h1>
+      <div className="project-grid">
+        {projects.map((project, index) => (
+          <Link to={`/portfolio/${index}`} key={index}>
+            <ProjectCard
+              image={project.image}
+              title={project.title}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
