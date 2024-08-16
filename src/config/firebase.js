83fd7e476cb564adc9d2122f28b930fdd9818e4a
ErasKuku <emensah670@st.ug.edu.gh>
2024-08-16 16:30:35 -0400

    restarting host

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,15 @@ import { getAuth} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyA696a4iNL_YpB54nv4XjTzo0VSMmMjr_M",
+  authDomain: "rameva-construction-f523b.firebaseapp.com",
+  projectId: "rameva-construction-f523b",
+  storageBucket: "rameva-construction-f523b.appspot.com",
+  messagingSenderId: "1003575592689",
+  appId: "1:1003575592689:web:06fe1800474c3636ba0b50",
+  measurementId: "G-BG5H5Q73M1"
+};
 
 
 
